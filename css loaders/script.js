e3ba7b292b251d18b0606b937992e61a3f7bf793
shaghaygh <shqayqm145@gmail.com>
2023-09-04 10:58:57 +0330

    4/9/2023 , 10:47

---
+++ css loaders/script.js
@@ -0,0 +1,26 @@
+// let dots = document.querySelectorAll('.dot');
+// // let num = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+// // let rand = Math.floor(Math.random() * 10);
+
+// // for (let i = 0; i < num.length; i++) {
+// //     dots.children[i].classList.add('anim');
+    
+// // }
+// dots.forEach(dot => {
+//     dot.addEventListener('click', 
+//     ()=>{
+//         dots.forEach((dot) => {
+//             dot.className="dot";
+//         })
+//         dot.classList.add('anim');
+//     })
+// });
+
+// // let rand = Math.floor(Math.random() * 10);
+
+// // setInterval(() => {
+// //     let rand = Math.floor(Math.random() * 10);
+// //     let rand1 = Math.floor(Math.random() * 100);
+// //     // dots.children[rand].classList.toggle('anim');
+// //     // dots.children[rand].style.transition = scale(rand1);
+// // }, 1000);
