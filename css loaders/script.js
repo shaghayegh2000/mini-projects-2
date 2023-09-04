3de8befaa8c5267fbb4f067416ec27ad63790a0c
shaghaygh <shqayqm145@gmail.com>
2023-09-04 12:26:23 +0330

    12:10 , 4/9/2023

--- css loaders/script.js
+++ css loaders/script.js
@@ -1,11 +1,11 @@
-// let dots = document.querySelectorAll('.dot');
-// // let num = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-// // let rand = Math.floor(Math.random() * 10);
+let dots = document.querySelectorAll('.dot');
+// let num = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+// let rand = Math.floor(Math.random() * 10);
 
-// // for (let i = 0; i < num.length; i++) {
-// //     dots.children[i].classList.add('anim');
+// for (let i = 0; i < num.length; i++) {
+//     dots.children[i].classList.add('anim');
     
-// // }
+// }
 // dots.forEach(dot => {
 //     dot.addEventListener('click', 
 //     ()=>{
@@ -16,11 +16,11 @@
 //     })
 // });
 
-// // let rand = Math.floor(Math.random() * 10);
+// let rand = Math.floor(Math.random() * 10);
 
-// // setInterval(() => {
-// //     let rand = Math.floor(Math.random() * 10);
-// //     let rand1 = Math.floor(Math.random() * 100);
-// //     // dots.children[rand].classList.toggle('anim');
-// //     // dots.children[rand].style.transition = scale(rand1);
-// // }, 1000);
+// setInterval(() => {
+//     let rand = Math.floor(Math.random() * 10);
+//     let rand1 = Math.floor(Math.random() * 100);
+//     // dots.children[rand].classList.toggle('anim');
+//     // dots.children[rand].style.transition = scale(rand1);
+// }, 1000);
