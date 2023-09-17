782c3a076a0576566842b57260b7f1270e448213
shaghaygh <shqayqm145@gmail.com>
2023-09-17 19:06:10 +0330

    17/9/2023 , 16:48

--- countDown/script.js
+++ countDown/script.js
@@ -1,20 +1,43 @@
 const countdown = document.querySelector(".countdown");
 
-setInterval(() => {
-    const deadline = new Date(2023, 9, 30, 12, 49, 50);
-    const current = new Date();
+const interval = setInterval(() => {
+  const deadline = new Date(2023, 9, 17, 14, 53, 55);
 
-    const diff = deadline - current;
+  const current = new Date();
 
-    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
-    const minutes = Math.floor((diff / (1000 * 60)) % 60);
-    const seconds = Math.floor((diff / 1000) % 60);
+  const diff = deadline - current;
 
-    countdown.innerHTML = `
-    <div>${days}</div>
-    <div>${hours}</div>
-    <div>${minutes}</div>
-    <div>${seconds}</div>
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24)) + "";
+  const hours = Math.floor((diff / (1000 * 60 * 60)) % 24) + "";
+  const minutes = Math.floor((diff / (1000 * 60)) % 60) + "";
+  const seconds = Math.floor((diff / 1000) % 60) + "";
+
+  countdown.innerHTML = `
+    <div data-content="Days">${days.length === 1 ? `0${days}` : days}</div>
+    <div data-content="Hours">${hours.length === 1 ? `0${hours}` : hours}</div>
+    <div data-content="Minutes">${
+      minutes.length === 1 ? `0${minutes}` : minutes
+    }</div>
+    <div data-content="Seconds">${
+      seconds.length === 1 ? `0${seconds}` : seconds
+    }</div>
 `;
+
+  if (diff < 0) {
+    clearInterval(interval);
+    countdown.innerHTML = "<h1>Here We Go!!!</h1>";
+  }
+
+  document.querySelector(".reset").addEventListener("click", () => {
+    clearInterval(interval);
+
+    const divs = document.querySelectorAll(".countdown div");
+
+    divs.forEach((div) => {
+      div.innerHTML = "00";
+    });
+  });
+  document.querySelector('.reset').addEventListener('click',()=>{
+    clearInterval(interval);
+  })
 }, 1000);
